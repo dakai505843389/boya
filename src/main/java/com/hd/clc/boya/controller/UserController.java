c3a149bf56cae474ff5d8a40723425d6334e8d41
dakai505843389 <1021641793@qq.com>
2017-12-25 10:42:05 +0800

    clc 提交

--- src/main/java/com/hd/clc/boya/controller/UserController.java
+++ src/main/java/com/hd/clc/boya/controller/UserController.java
@@ -3,7 +3,9 @@ package com.hd.clc.boya.controller;
 import com.hd.clc.boya.common.Result;
 import com.hd.clc.boya.common.ResultDetial;
 import com.hd.clc.boya.common.WxUtil;
+import com.hd.clc.boya.service.impl.UserServiceImpl;
 import com.hd.clc.boya.vo.UserCode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,12 +17,20 @@ import java.util.Map;
 @RestController
 public class UserController {
 
+    @Autowired
+    private UserServiceImpl userService;
+
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public Result login(@RequestParam String code, HttpServletRequest request){
         Map<String, Object> data = new HashMap<>();
         UserCode userCode = WxUtil.getUserCode(code);
         data.put("userCode", userCode);
         return new ResultDetial<>(data);
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    public Result test(@RequestParam Integer id){
+        return userService.query(id);
     }
 
 }
