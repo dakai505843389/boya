71d89cbc419e404a832b2c4795439939acf6dec2
FUYUEHAO <13588014652@163.com>
2017-12-29 16:39:06 +0800

    fyh 2017年12月29日16:39:00

---
+++ src/main/java/com/hd/clc/boya/db/entity/Manager.java
@@ -0,0 +1,12 @@
+package com.hd.clc.boya.db.entity;
+
+import java.util.Date;
+
+public class Manager {
+    private Integer id;
+    private String  account;
+    private String  password;
+    private Integer managerType;
+    private Date addTime;
+    private Date lastLoginTime;
+}
